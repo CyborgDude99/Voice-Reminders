2aa0e40aa491eafc26474fd39f1d0f92cc535561
Alex <cyborgdude99@gmail.com>
2026-03-14 13:23:20 +1100

    Reworked timer scripts and added an escalated state.

--- VoiceReminder/wakeup.py
+++ VoiceReminder/wakeup.py
@@ -18,9 +18,13 @@ from pygame import mixer
 # Variables
 ModelName = "./glados.onnx"
 Voiceline1 = "We hope you have had a wonderful rest! Time to face the morning rush. Please wake up and go eat some food."
-WavFile = "wakeup.wav"
+Voiceline2 = "Hurry up, meatbag."
+WavFile1 = "wakeup.wav"
+WavFile2 = "meatbag.wav"
 INITIALSTATE = 1
 WAITINGINITIALSTATE = 2
+ESCALATEDSTATE = 3
+WAITINGESCALATEDSTATE = 4
 SHUTDOWNSTATE = 99
 CurrentState = INITIALSTATE
 
@@ -38,21 +42,32 @@ logger = logging.getLogger(__name__)
 running = True
 
 # Generate wav file
-with wave.open(WavFile, "wb") as wav_file:
+with wave.open(WavFile1, "wb") as wav_file:
         voice.synthesize_wav(Voiceline1, wav_file)
+with wave.open(WavFile2, "wb") as wav_file:
+        voice.synthesize_wav(Voiceline2, wav_file)
 
-start = timer()
 while running: 
         if CurrentState == INITIALSTATE:
                 logger.info("In initial state")
-                mixer.music.load(WavFile)
+                mixer.music.load(WavFile1)
                 mixer.music.play()
+                start = timer()
                 CurrentState = WAITINGINITIALSTATE
         if CurrentState == WAITINGINITIALSTATE:
                 # Has enough time passed?
-                logger.info("In waiting initial state")
                 if timer() - start >= 10:
-                        CurrentState = SHUTDOWNSTATE 
+                        CurrentState = ESCALATEDSTATE
+        if CurrentState == ESCALATEDSTATE: 
+                logger.info("In escalated state")
+                mixer.music.load(WavFile2)
+                mixer.music.play()
+                start = timer()
+                CurrentState = WAITINGESCALATEDSTATE
+        if CurrentState == WAITINGESCALATEDSTATE:
+                # Has enough time passed?
+                if timer() - start >= 10:
+                        CurrentState = SHUTDOWNSTATE
         if CurrentState == SHUTDOWNSTATE:
                 logger.info("In shutdown state")
                 running = False                           
